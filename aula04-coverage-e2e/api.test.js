a3dbb4f6decc85a8c28559ddaefe1dbf6ef78efe
Paulo Ricardo <pauloricardo.m.almeida@gmail.com>
2022-10-05 09:13:51 -0300

    aula 04 e2e coverage

---
+++ aula04-coverage-e2e/api.test.js
@@ -0,0 +1,10 @@
+const { describe, it } = require('mocha')
+const request = require('supertest')
+
+describe('API Suite test', () => {
+  describe('/contact', () => {
+    it('should request the contact page and return HTTP Status 200', async() => {
+      const response = await request()
+    })
+  })
+})
