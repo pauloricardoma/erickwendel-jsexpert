b42f419aee6b208c5d6adeeef44d6c029541e294
Paulo Ricardo <pauloricardo.m.almeida@gmail.com>
2022-10-08 23:16:09 -0300

    aula 4 coverage e2e

--- aula04-coverage-e2e/api.js
+++ aula04-coverage-e2e/api.js
@@ -1,9 +1,26 @@
 const http = require('http')
-
+const DEFAULT_USER = { username: "ErickWendel", password: "123" }
 const routes = {
   '/contact:get': (request, response) => {
     response.write('contact us page')
     return response.end()
+  },
+
+  '/login:post': async (request, response) => {
+    // response é um async iterator!
+    for await (const data of request) {
+      const user = JSON.parse(data)
+      if (
+        user.username !== DEFAULT_USER.username ||
+        user.password !== DEFAULT_USER.password
+      ) {
+        response.writeHead(401)
+        response.write("Logging failed!")
+        return response.end()
+      }
+      response.write("Logging has succeeded!")
+      return response.end()
+    }
   },
 
   default: (request, response) => {
