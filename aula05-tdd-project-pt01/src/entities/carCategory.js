f823db2e71218b04f219909aa00705aa54634a3a
Paulo Ricardo <pauloricardo.m.almeida@gmail.com>
2022-10-09 16:17:45 -0300

    aula 5 project part 1

---
+++ aula05-tdd-project-pt01/src/entities/carCategory.js
@@ -0,0 +1,19 @@
+const Base = require('./base/base')
+
+class CarCategory extends Base {
+  constructor({ 
+    id, 
+    name, 
+    carIds, 
+    price, 
+  }) {
+    super({ id, name })
+
+    this.carIds = carIds  
+    this.price = price 
+  }
+
+  
+}
+
+module.exports = CarCategory
